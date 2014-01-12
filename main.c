fb8a3b62e72c6a575a3ea1ac1004c6017a72d852
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2014-01-12 16:53:43 +0800

    wrote a template to load input in C

---
+++ main.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+#include<string.h>
+
+int main(void){
+    FILE * fp = fopen("/home/abeaulne/codejam/SaveHumanity/sample.in", "r");
+    int num_cases;
+    fscanf(fp, "%d\n", &num_cases);
+
+    int i;
+    for(i=0; i<num_cases; ++i){
+        char input1[10];
+        char input2[10];
+        fscanf(fp, "%s\n%s\n\n", input1, input2);
+        printf("input 1: %s\ninput 2: %s\n\n", input1, input2);
+    }
+
+    fclose(fp);
+}
+
