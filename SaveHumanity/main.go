c3ba077d2095adc935d8e3a4f3a48a3493cb8a95
Alexandre Beaulne <abeaulne@eclipseoptions.com>
2014-02-25 21:57:14 +0800

    chugging along

--- SaveHumanity/main.go
+++ SaveHumanity/main.go
@@ -1,55 +1,96 @@
 package main
 
 import "fmt"
-//import "os"
+
+//func main() {
+//
+//    var numCases int
+//    _, err := fmt.Scanf("%d\n", &numCases)
+//    if err != nil { panic(err) }
+//
+//    fmt.Printf("numcases: %v\n", numCases)
+//
+//    for i := 0; i < numCases; i++ {
+//        var text, pattern string
+//        _, err = fmt.Scanf("%v\n%v\n", &text, &pattern)
+//        if err != nil { panic(err) }
+//        _, err = fmt.Scanf("\n")
+//        if err != nil { panic(err) }
+//
+//        skipTbl := skipTable(pattern)
+//        solve(text, pattern, &skipTbl)
+//    }
+//}
+//
+//func solve(text, pattern string, skipTbl *[]int) {
+//    plen := len(pattern)
+//    for i := 0; i <= len(text)-plen; {
+//        mismatch := false
+//        match := true
+//        var j int;
+//        for j=0; j<plen; j++ {
+//            if pattern[j] != text[i+j] {
+//                if mismatch {
+//                    match = false
+//                    j++
+//                    break
+//                } else {
+//                    mismatch = true
+//                }
+//            }
+//        }
+//
+//        if match {
+////            fmt.Printf("%d ", i)
+//            fmt.Printf("\nMatch attempt (i=%d, j=%d, ", i, j)
+//            fmt.Printf("inc=%d, next=%d) YES:\n", (*skipTbl)[j-1], i + (*skipTbl)[j-1])
+//            fmt.Printf("%s\n%s\n%v\n", text, leftpad(pattern, i+plen), *skipTbl)
+//        } else {
+//            fmt.Printf("\nMatch attempt (i=%d, j=%d, ", i, j)
+//            fmt.Printf("inc=%d, next=%d) NO:\n", (*skipTbl)[j-1], i + (*skipTbl)[j-1])
+//            fmt.Printf("%s\n%s\n%v\n", text, leftpad(pattern, i+plen), *skipTbl)
+//        }
+//        i += (*skipTbl)[j-1]
+//    }
+//    fmt.Printf("\n")
+//}
 
 func main() {
-
-    var numCases int
-    _, err := fmt.Scanf("%d\n", &numCases)
-    if err != nil { panic(err) }
-
-    fmt.Printf("numcases: %v\n", numCases)
-
-    for i := 0; i < numCases; i++ {
-        var text, pattern string
-        _, err = fmt.Scanf("%v\n%v\n", &text, &pattern)
-        if err != nil { panic(err) }
-        _, err = fmt.Scanf("\n")
-        if err != nil { panic(err) }
-
-        fmt.Printf("Case %d: text: %s, pattern: %s, skipTable: %v\n", i, text, pattern, skipTable(pattern))
-    }
+	str := "abcdefabcdefabcdefghiabcdef"
+	fmt.Printf("\nskipTable(\"%s\"):\n%v\n", str, skipTable(str))
 }
 
 func skipTable(pattern string) []int {
-    length := len(pattern)
-    fss_ := fss(pattern, length)
-    ret := make([]int, length)
-    for i:=0; i<fss_; i++ {
-        ret[i] = i
-    }
-    for i:=fss_; i<length; i++ {
-        ret[i] = fss_
-    }
-    return ret
-}
-
-// First Similar Suffix, one mismatch allowed
-func fss(pattern string, length int) int {
-    for i:=1; i<length-1; i++ {
-        mismatchUsed := false
-        match := true
-        for j:=0; j<length-i && match; j++ {
-            if pattern[j] != pattern[i+j] {
-                if mismatchUsed {
-                    match = false
+    fmt.Printf("\n%s\n", "abcdefabcdefabcdefghiabcdef")
+    for i:=1; i<len(pattern); i++ {
+        fmt.Printf("%s", pattern[i:])
+        mismatch := false
+        var j int
+        var v rune
+        for j, v = range pattern[i:] {
+            if v != rune(pattern[j]) {
+                if mismatch {
+                    break
                 } else {
-                    mismatchUsed = true
+                    mismatch = true
                 }
             }
         }
-        if match { return i }
+        fmt.Printf("      %d\n", j)
     }
-    return length-1
+
+    return []int{}
+}
+
+func leftpad(str string, num int) string {
+    format := fmt.Sprintf("%%%ds", num)
+    return fmt.Sprintf(format, str)
+}
+
+func min(a, b int) int {
+    if a<b {
+        return a
+    } else {
+        return b
+    }
 }
