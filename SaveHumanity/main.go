3c699552e589ec0ddee6181d895bce6e608ba97b
Alexandre Beaulne <abeaulne@eclipseoptions.com>
2014-03-09 19:42:37 +0800

    main.go

--- SaveHumanity/main.go
+++ SaveHumanity/main.go
@@ -2,97 +2,98 @@ package main
 
 import "fmt"
 
-//func main() {
-//
-//    var numCases int
-//    _, err := fmt.Scanf("%d\n", &numCases)
-//    if err != nil { panic(err) }
-//
-//    fmt.Printf("numcases: %v\n", numCases)
-//
-//    for i := 0; i < numCases; i++ {
-//        var text, pattern string
-//        _, err = fmt.Scanf("%v\n%v\n", &text, &pattern)
-//        if err != nil { panic(err) }
-//        _, err = fmt.Scanf("\n")
-//        if err != nil { panic(err) }
-//
-//        skipTbl := skipTable(pattern)
-//        solve(text, pattern, &skipTbl)
-//    }
-//}
-//
-//func solve(text, pattern string, skipTbl *[]int) {
-//    plen := len(pattern)
-//    for i := 0; i <= len(text)-plen; {
-//        mismatch := false
-//        match := true
-//        var j int;
-//        for j=0; j<plen; j++ {
-//            if pattern[j] != text[i+j] {
-//                if mismatch {
-//                    match = false
-//                    j++
-//                    break
-//                } else {
-//                    mismatch = true
-//                }
-//            }
-//        }
-//
-//        if match {
-////            fmt.Printf("%d ", i)
-//            fmt.Printf("\nMatch attempt (i=%d, j=%d, ", i, j)
-//            fmt.Printf("inc=%d, next=%d) YES:\n", (*skipTbl)[j-1], i + (*skipTbl)[j-1])
-//            fmt.Printf("%s\n%s\n%v\n", text, leftpad(pattern, i+plen), *skipTbl)
-//        } else {
-//            fmt.Printf("\nMatch attempt (i=%d, j=%d, ", i, j)
-//            fmt.Printf("inc=%d, next=%d) NO:\n", (*skipTbl)[j-1], i + (*skipTbl)[j-1])
-//            fmt.Printf("%s\n%s\n%v\n", text, leftpad(pattern, i+plen), *skipTbl)
-//        }
-//        i += (*skipTbl)[j-1]
-//    }
-//    fmt.Printf("\n")
-//}
+// Modified KMP algorithm
 
 func main() {
-	str := "abcdefabcdefabcdefghiabcdef"
-	fmt.Printf("\nskipTable(\"%s\"):\n%v\n", str, skipTable(str))
+
+    var numCases int
+    _, err := fmt.Scanf("%d\n", &numCases)
+    if err != nil { panic(err) }
+
+    fmt.Printf("numcases: %v\n", numCases)
+
+    for i := 0; i < numCases; i++ {
+        var text, pattern string
+        _, err = fmt.Scanf("%v\n%v\n", &text, &pattern)
+        if err != nil { panic(err) }
+        _, err = fmt.Scanf("\n")
+        if err != nil { panic(err) }
+
+        fmt.Printf("\ntext:    %v\npattern: %v\n", text, pattern)
+        fmt.Printf("shifts: %v\n", fuzzyKMP(text, pattern, 1))
+    }
 }
 
-func skipTable(pattern string) []int {
-//    fmt.Printf("\n%s\n", pattern)
-    skipTable :+ make([]int, len(pattern))
-    for i:=1; i<len(pattern); i++ {
-//        fmt.Printf("%s", pattern[i:])
-        mismatch := false
-        var j int
-        var v rune
-        for j, v = range pattern[i:] {
-            if v != rune(pattern[j]) {
-                if mismatch {
-                    break
-                } else {
-                    mismatch = true
-                }
+func fuzzyKMP(text string, pattern string, k int) []int {
+    n := len(text)
+    m := len(pattern)
+    result := make([]int, 0, n)
+    pi := computeFuzzyPrefix(pattern, k)
+    q := 0
+
+    for i := 0; i < n-m+1; i++ {
+        match := true
+        for q < m && i+q < n {
+            if pattern[q] == text[i+q] {
+                q++
+            } else {
+                q = pi[q]
+                match = false
+                break
             }
         }
-//        fmt.Printf("      %d\n", j)
-        skipTable[i] = j
+        if match {
+            result = append(result, i)
+        }
+    }
+    return result
+}
+
+func computeFuzzyPrefix(pattern string, k int) []int {
+    // place for improvment; worst case O(m^3), maybe possible in O(m)
+    m := len(pattern)
+    pi := make([]int, m)
+    for i := 1; i <= m; i++ {
+        var j int
+        for j = 1; j <= i; j++ {
+            if fuzzyCompare(pattern[j:i], pattern[:i-j], k) {
+                break
+            }
+        }
+        pi[i-1] = i-j
+    }
+    return pi
+}
+
+func fuzzyCompare(a string, b string, k int) bool {
+    if len(a) != len(b) {
+        panic("in func compare; strings are of different length!")
     }
 
-    return skipTable
-}
-
-func leftpad(str string, num int) string {
-    format := fmt.Sprintf("%%%ds", num)
-    return fmt.Sprintf(format, str)
-}
-
-func min(a, b int) int {
-    if a<b {
-        return a
-    } else {
-        return b
+    for i, v := range a {
+        if rune(b[i]) != v {
+            if k == 0 {
+                return false
+            } else {
+                k--
+            }
+        }
     }
+
+    return true
 }
+
+
+
+//func leftpad(str string, num int) string {
+//    format := fmt.Sprintf("%%%ds", num)
+//    return fmt.Sprintf(format, str)
+//}
+//
+//func min(a, b int) int {
+//    if a<b {
+//        return a
+//    } else {
+//        return b
+//    }
+//}
