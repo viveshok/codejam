6b2ac3eec60f8a9cb51dc84aea6d577f7b9f2a87
Alexandre Beaulne <abeaulne@eclipseoptions.com>
2014-03-11 21:30:03 +0800

    finally some progress

--- SaveHumanity/main.go
+++ SaveHumanity/main.go
@@ -1,11 +1,9 @@
 package main
 
-// Using suffix tree, credits to Prof. Carl Kingsford, CMU
 import "fmt"
 
 type Node struct {
     children map[rune]*Node
-    suffixLink *Node
 }
 
 func newNode() *Node {
@@ -14,45 +12,54 @@ func newNode() *Node {
     return n
 }
 
-func newNodeWithSuffix(suffixLink *Node) *Node {
-    n := newNode()
-    n.suffixLink = suffixLink
-    return n
-}
-
 func buildSuffixTrie(s string) *Node {
     if len(s) == 0 {
         panic("Can't build suffix tree of empty string!")
     }
+
+    s += "$"
     root := newNode()
-    root.suffixLink = root
-    longest := newNodeWithSuffix(root)
-    root.children[rune(s[0])] = longest
 
-    for _, c := range s[1:] {
-        current := longest
-        var previous *Node
-        for _, present := current.children[c]; !present; _, present = current.children[c] {
-            r1 := newNode()
-            current.children[c] = r1
-
-            if previous != nil {
-                previous.suffixLink = r1
+    for i := range s {
+        current := root
+        for _, c := range s[i:] {
+            if _, ok := current.children[c]; !ok {
+                current.children[c] = newNode()
             }
-
-            previous = r1
-            current = current.suffixLink
+            current = current.children[c]
         }
-
-        if current == root {
-            previous.suffixLink = root
-        } else {
-            previous.suffixLink = current.children[c]
-        }
-        longest = longest.children[c]
     }
-
     return root
+}
+
+// return length of all suffixes in sub-trie
+func getIndices(n *Node, k int) []int {
+    result := []int{}
+    for c, child := range n.children {
+        if c == '$' {
+            result = append(result, k)
+        } else {
+            result = append(result, getIndices(child, k+1)...)
+        }
+    }
+    return result
+}
+
+// return indices of match (from the end, end of pattern!)
+func find(n *Node, p string, k int) []int {
+    if k < 0 { // used all mismatch
+        return []int{}
+    } else if len(p) == 0 { // match!
+        return getIndices(n, 0)
+    } else if next, ok := n.children[rune(p[0])]; ok {
+        return find(next, p[1:], k)
+    } else { // burn a mismatch
+        result := []int{}
+        for _, child := range n.children {
+            result = append(result, find(child, p[1:], k-1)...)
+        }
+        return result
+    }
 }
 
 func main() {
@@ -64,11 +71,40 @@ func main() {
 
     for i := 0; i < numCases; i++ {
         var text, pattern string
-        _, err = fmt.Scanf("%v\n%v\n\n", &text, &pattern)
+        _, err = fmt.Scanf("%v\n%v\n", &text, &pattern)
+        if err != nil { panic(err) }
+        _, err = fmt.Scanf("\n")
         if err != nil { panic(err) }
 
-        fmt.Printf("\ntext:    %v\npattern: %v\n", text, pattern)
-        fmt.Printf("suffix trie: %v\n", buildSuffixTrie(text))
+        for _, i := range find(buildSuffixTrie(text), pattern, 1) {
+            fmt.Printf("\ntext:    %v\n", text)
+            shift := len(text)-len(pattern)-i
+            fmt.Printf("pattern: %v\n", leftpad(pattern, shift))
+//            fmt.Printf("%v ", shift)
+        }
+        fmt.Printf("\n")
     }
 }
 
+func prettyPrint(n *Node) string {
+    result := "["
+    i := 1
+    for c, child := range n.children {
+        if c == '$' {
+            result += string(c)
+        } else {
+            result += string(c) + ":" + prettyPrint(child)
+        }
+
+        if i < len(n.children) {
+            result += ", "
+        }
+        i++
+    }
+    return result + "]"
+}
+
+func leftpad(str string, num int) string {
+    format := fmt.Sprintf("%%%ds", num+len(str))
+    return fmt.Sprintf(format, str)
+}
