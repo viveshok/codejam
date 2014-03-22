0f68c760cbdad93a9554deb64a5d9a7c98543b0d
Alexandre Beaulne <abeaulne@eclipseoptions.com>
2014-03-22 16:36:18 +0800

    trying smtg new with oil well

--- OilWell/main.py
+++ OilWell/main.py
@@ -1,48 +1,41 @@
 #!/usr/bin/python3
 
 import sys
-import collections
 
-def acme_distance(a, b):
-    return max(abs(b[0]-a[0]), abs(b[1]-a[1]))
+def cull_xs(xs, x
 
-def cull(vertices, p):
-    result = dict()
-    for k, vs in vertices.items():
-        non_overlapping = [v for v in vs if p not in v]
-        if non_overlapping:
-            result[k] = non_overlapping
-    return result
-
-def solve(vertices, acc):
-    if vertices:
-        solns = []
-        dist = max(vertices.keys())
-        for (p0, p1) in vertices[dist]:
-            solns.append(solve(cull(vertices, p0), acc+dist))
-            solns.append(solve(cull(vertices, p1), acc+dist))
-        return min(solns)
+def solve(xs, ys, acc):
+    if xs:
+        max_dist_x = xs[-1] - xs[0]
+        max_dist_y = ys[-1] - ys[0]
+        if max_dist_x >= max_dist_y:
+            left = solve(xs[:-1], cull(ys, xs[-1]), acc+max_dist_x)
+            right = solve(xs[1:], cull(ys, xs[0]), acc+max_dist_x)
+            return min(left, right)
+        else:
+            left = solve(cull_xs(xs, ys[-1]), ys[:-1], acc+max_dist_x)
+            right = solve(cull(xs, ys[0]), ys[1:], acc+max_dist_x)
+            return min(left, right)
     else:
         return acc
-
-def get_vertices(points):
-    vertices = collections.defaultdict(list)
-    while points:
-        p0 = points.pop()
-        for p in points:
-            vertices[acme_distance(p0, p)].append({p0, p})
-
-    return vertices
 
 if __name__ == "__main__":
 
     [r, c] = [int(X) for X in sys.stdin.readline().split()]
 
-    points = set()
+    xs = []
+    ys = []
+    locations_xs = {}
+    locations_ys = {}
+
     for i in range(r):
         for j, char in enumerate(sys.stdin.readline().split()):
             if char == '1':
-                points.add((i,j))
+                xs.append(i)
+                ys.append(j)
+                location_xs(j) = i
+                location_ys(i) = j
+                POINTS.append((i,j))
 
-    print(solve(get_vertices(points), 0))
+    print(solve(xs, sorted(ys), locations_xs, locations_ys, 0))
 
