b1019c17a57be5f1dc27b2b3e86c6e3c6b03e850
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2014-03-22 22:38:59 +0800

    playing with ideas

--- OilWell/main.py
+++ OilWell/main.py
@@ -1,41 +1,35 @@
 #!/usr/bin/python3
 
 import sys
+import collections
 
-def cull_xs(xs, x
+def acme_distance(a, b):
+    return max(abs(b[0]-a[0]), abs(b[1]-a[1]))
 
-def solve(xs, ys, acc):
-    if xs:
-        max_dist_x = xs[-1] - xs[0]
-        max_dist_y = ys[-1] - ys[0]
-        if max_dist_x >= max_dist_y:
-            left = solve(xs[:-1], cull(ys, xs[-1]), acc+max_dist_x)
-            right = solve(xs[1:], cull(ys, xs[0]), acc+max_dist_x)
-            return min(left, right)
-        else:
-            left = solve(cull_xs(xs, ys[-1]), ys[:-1], acc+max_dist_x)
-            right = solve(cull(xs, ys[0]), ys[1:], acc+max_dist_x)
-            return min(left, right)
-    else:
-        return acc
+def get_vertices(points):
+    vertices = collections.defaultdict(list)
+    while points:
+        p0 = points.pop()
+        for p1 in points:
+            vertices[acme_distance(p0, p1)].append((p0, p1))
+
+    return vertices
 
 if __name__ == "__main__":
 
     [r, c] = [int(X) for X in sys.stdin.readline().split()]
 
-    xs = []
-    ys = []
-    locations_xs = {}
-    locations_ys = {}
+    points = []
 
     for i in range(r):
         for j, char in enumerate(sys.stdin.readline().split()):
             if char == '1':
-                xs.append(i)
-                ys.append(j)
-                location_xs(j) = i
-                location_ys(i) = j
-                POINTS.append((i,j))
+                points.append((i, j))
 
-    print(solve(xs, sorted(ys), locations_xs, locations_ys, 0))
+    tmp = 0
+    for k, v in get_vertices(points).items():
+        print("k:",k,", len(v):", len(v))
+        tmp += len(v)
+
+    print("tmp: ", tmp)
 
