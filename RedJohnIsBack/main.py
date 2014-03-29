5f3205f5ffb499bc16f5b699a74f225970c94169
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2014-03-29 15:12:12 +0800

    got red john is back

--- RedJohnIsBack/main.py
+++ RedJohnIsBack/main.py
@@ -16,38 +16,40 @@ def num_ways(N, values):
     return result
 
 def sieve(N):
-    numbers = [(i, True) for i in range(2, N+1)]
-    p = 2
-    counter = p+p
-    while p < N:
-        while counter <= N:
-            numbers[counter-2] = (counter, False)
-            counter += p
-        
-        p = next((i for i, v in numbers[p:] if v), N) 
-        counter = p+p
+    ledger = [True] * (N+1) 
+    ledger[0] = False # 0 is not prime
+    ledger[1] = False # 1 is not prime
 
-    return [i for i, v in numbers if v]
+    primes = []
+    while True:
+        try:
+            next_prime = next(i for i, v in enumerate(ledger) if v)
+            primes.append(next_prime)
+            for i in range(next_prime, N+1, next_prime):
+                ledger[i] = False
+        except StopIteration:
+            break
 
+    return primes
 
 if __name__ == "__main__":
 
-    ## find number of ways to build wall, memoize
-    #num_ways_ = dict()
-    #for i in range(1, 41):
-    #    num_ways_[i] = num_ways(i, [1, 4])
-    #print("num_ways_ =", num_ways_)
+#    # find number of ways to build wall, memoize
+#    num_ways_ = dict()
+#    for i in range(1, 41):
+#        num_ways_[i] = num_ways(i, [1, 4])
+#
+#    # find all primes up to num_ways_[40],
+#    # using sieve of eratosthenes
+#    primes = sieve(num_ways_[40])
+#
+#    # compute answer for all possible N = 1, ..., 40, memoize
+#    num_primes = dict()
+#    for i, v in num_ways_.items():
+#        num_primes[i] = len([x for x in primes if x <= v])
+#    print("num_primes =", num_primes)
 
-    ## find all primes up to num_ways_[40],
-    ## using sieve of eratosthenes
-    #primes = sieve(num_ways_[40])
-
-    ## compute answer for all possible N = 1, ..., 40, memoize
-    #num_primes = dict()
-    #for i, v in num_ways_.items():
-    #    num_primes[i] = len([x for x in primes if x <= v])
-
-    num_primes = {1: 0, 2: 0, 3: 0, 4: 1, 5: 2, 6: 2, 7: 3, 8: 4, 9: 5, 10: 7, 11: 9, 12: 10, 13: 12, 14: 16, 15: 20, 16: 25, 17: 33, 18: 43, 19: 54, 20: 69, 21: 92, 22: 120, 23: 156, 24: 205, 25: 270, 26: 355, 27: 463, 28: 616, 29: 817, 30: 1078, 31: 1433, 32: 1913, 33: 2544, 34: 3386, 35: 4523, 36: 6049, 37: 8079, 38: 10795, 39: 14476, 40: 19386}
+    num_primes = {1: 0, 2: 0, 3: 0, 4: 1, 5: 2, 6: 2, 7: 3, 8: 4, 9: 4, 10: 6, 11: 8, 12: 9, 13: 11, 14: 15, 15: 19, 16: 24, 17: 32, 18: 42, 19: 53, 20: 68, 21: 91, 22: 119, 23: 155, 24: 204, 25: 269, 26: 354, 27: 462, 28: 615, 29: 816, 30: 1077, 31: 1432, 32: 1912, 33: 2543, 34: 3385, 35: 4522, 36: 6048, 37: 8078, 38: 10794, 39: 14475, 40: 19385}
 
     T = int(sys.stdin.readline())
 
