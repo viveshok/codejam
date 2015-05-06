16cf2c027ac02aaebaf7d9403200f1409776a71a
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2015-05-06 22:43:04 +0800

    recursive to iterative swap nodes

--- SwapNodes/main.py
+++ SwapNodes/main.py
@@ -8,6 +8,19 @@ import sys
 #import math
 #import collections
 
+def step(node):
+    left = traverse(node.left)
+    left.append(node.val)
+    right = traverse(node.right)
+    left.extend(right)
+    return left
+
+def traverse(node):
+    if node:
+        return step(node)
+    else:
+        return []
+
 class Node:
 
     def __init__(self, val):
@@ -22,11 +35,11 @@ class Node:
         self.right = r
 
     def traverse(self):
-        if self.left:
-            self.left.traverse()
-        print(self.val, end=" ")
-        if self.right:
-            self.right.traverse()
+        left = self.left.traverse() if self.left else []
+        left.append(self.val)
+        right = self.right.traverse() if self.right else []
+        left.extend(right)
+        return left
 
     def swap(self, i, K):
         if i == K:
@@ -43,8 +56,9 @@ if __name__ == "__main__":
 
     # try to come up with iterative instead
     # of recursive solution to stop blowing
-    # the stack
-    sys.setrecursionlimit(2000)
+    # the stack - consult:
+    # http://blog.moertel.com/posts/2013-06-03-recursion-to-iteration-3.html
+    sys.setrecursionlimit(4000)
 
     N = int(sys.stdin.readline())
     nodes = [Node(i) for i in range(1, N+1)]
@@ -61,6 +75,11 @@ if __name__ == "__main__":
     for case in range(T):
         K = int(sys.stdin.readline())
         root.swap(1, K)
-        root.traverse()
+
+        vals = traverse(root)
+#        vals = root.traverse()
+
+        for val in vals:
+            print(val, end=" ")
         print()
 
