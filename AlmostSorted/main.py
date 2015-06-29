337d2449fbf27e872a0596bf1dd27a8d8ec46b60
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2015-06-29 22:21:00 +0800

    finished almost sorted

--- AlmostSorted/main.py
+++ AlmostSorted/main.py
@@ -8,6 +8,47 @@ import sys
 #import math
 #import collections
 
+def is_sorted(ds):
+    i = 0
+    while i < len(ds)-1:
+        if ds[i] > ds[i+1]:
+            return False
+        i += 1
+    return True
+
+def swap(i, j, ds):
+    result = [d for d in ds]
+    tmp = result[i]
+    result[i] = result[j]
+    result[j] = tmp
+    return result
+
+def solve_swap(ds):
+    i = 0
+    candidate_idx = None
+    candidate = None
+    candidate_left_neighbour = None
+    candidate_right_neighbour = None
+
+    while i<len(ds)-1:
+        if not candidate and ds[i] > ds[i+1]:
+            candidate_idx = i
+            candidate = ds[i]
+            candidate_left_neighbour = -1 if i==0 else ds[i-1]
+            candidate_right_neighbour = ds[i+1]
+
+        if candidate and ds[i] > ds[i+1]:
+            if candidate_left_neighbour < ds[i+1] < candidate_right_neighbour:
+                tmp = ds[i+2] if i+2<len(ds) else 1000001
+                if ds[i] < candidate < tmp:
+                    test = swap(candidate_idx, i+1, ds)
+                    if is_sorted(test):
+                        return "yes\nswap {} {}".format(candidate_idx+1, i+1+1)
+
+        i += 1
+
+    return False
+
 class Segment():
     # direction: -1 -> decreasing, 0 -> neither, 1 -> increasing
 
@@ -20,11 +61,11 @@ class Segment():
     def append(self, item):
         self.content.append(item)
         self.min_ = item if item<self.min_ else self.min_
-        self.max_ = item if item<self.max_ else self.max_
+        self.max_ = item if item>self.max_ else self.max_
 
     def __str__(self):
-        fmt = "direction: {} min: {} max: {}"
-        return fmt.format(self.direction, self.min_, self.max_)
+        fmt = "content: {} direction: {} min: {} max: {}"
+        return fmt.format(self.content, self.direction, self.min_, self.max_)
 
 def chop_segment(ds):
 
@@ -70,15 +111,19 @@ def get_segments(ds):
             return False
     return segments
 
-def solve(segments):
+def get_indices_reverse_segment(segments):
+    count = 1
+    for segment in segments:
+        if segment.direction == -1:
+            return (count, count+len(segment.content)-1)
+        else:
+            count += len(segment.content)
 
-    for s in segments:
-        print(s)
+def solve_reverse(segments):
 
     # first check if array is already sorted
     if len(segments) == 1 and segments[0].direction != -1:
-        print("yes")
-        return
+        return "yes"
 
     # second check if it's possible to reverse one segment
     num_reversed = sum([s.direction == -1 for s in segments])
@@ -91,18 +136,12 @@ def solve(segments):
                 break
             i += 1
         if segments_ordered:
-            print("yes")
-            # TODO
-            print("reverse {} {}".format("?", "?"))
-            return
+            (start, stop) = get_indices_reverse_segment(segments)
+            return "yes\nreverse {} {}".format(start, stop)
 
-    # finally check if possible to swap two 1-long segments
-    # TODO
-    # if ...
-    # ...
-    # return
+    return False
 
-    print("no")
+
 
 if __name__ == "__main__":
 
@@ -110,10 +149,25 @@ if __name__ == "__main__":
 
     ds = [int(X) for X in sys.stdin.readline().split()]
 
+    if len(ds) == 2:
+        if ds[0] < ds[1]:
+            print("yes")
+        else:
+            print("yes\nswap 1 2")
+        exit(0)
+
+    result = solve_swap(ds)
+    if result:
+        print(result)
+        exit(0)
+
     segments = get_segments(ds)
 
     if segments:
-        solve(segments)
-    else:
-        print("no")
+        result = solve_reverse(segments)
+        if result:
+            print(result)
+            exit(0)
+
+    print("no")
 
