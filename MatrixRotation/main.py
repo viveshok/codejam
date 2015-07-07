50f586aaaa717534aaeaa0f8d46b26b0cf7ed96e
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2015-07-08 00:06:39 +0800

    matrix rotation: new approach

--- MatrixRotation/main.py
+++ MatrixRotation/main.py
@@ -5,89 +5,70 @@
 # $ ./main.py < input > output # to run
 
 import sys
-import math
+#import math
 #import collections
-
-def memoize(function):
-    cache = dict()
-    def memoized_function(*args):
-        if args in cache:
-            return cache[args]
-        else:
-            result = function(*args)
-            cache[args] = result
-            return result
-    return memoized_function
-
-@memoize
-def range_(M, N, L):
-    return 2 * L + (N-M if M<N else 0)
 
 if __name__ == "__main__":
 
     [M, N, R] = [int(X) for X in sys.stdin.readline().split()]
 
-    NUM_LAYERS = min(M, N)/2
-    THICKNESS_CORE_X = N/2 - NUM_LAYERS + 1
-    THICKNESS_CORE_Y = M/2 - NUM_LAYERS + 1
-
-    def layer(i, j, M, N):
-        '''
-        >>> layer(0, 0, 4, 5)
-        2
-        >>> layer(0, 2, 4, 5)
-        2
-        >>> layer(1, 1, 4, 5)
-        1
-        >>> layer(1, 2, 4, 5)
-        1
-        >>> layer(0, 3, 4, 5)
-        2
-        >>> layer(1, 3, 4, 5)
-        1
-        >>> layer(0, 0, 2, 6)
-        1
-        '''
-
-        distance_x = N/2-j if j<N/2 else j-N/2+1
-        layer_x = max(distance_x-THICKNESS_CORE_X+1, 1)
-
-        distance_y = M/2-i if i<M/2 else i-M/2+1
-        layer_y = max(distance_y-THICKNESS_CORE_Y+1, 1)
-
-        return round(max(layer_x, layer_y))
-
-    def shift(i, j, M, N, R):
-        L = layer(i, j, M, N)
-        r_x = range_(M, N, L)
-        r_y = range_(N, M, L)
-        R %= 2*r_x + 2*r_y - 4
-        while R > 0:
-            if M//2-i == L and j<N//2+L-1: # shift right
-                delta = min(math.ceil(N/2)+L-j-1, R)
-                j += delta
-                R -= delta
-            elif j-math.ceil(N/2)+1 == L and i<math.ceil(M/2)+L-1: # shift down
-                delta = min(math.ceil(M/2)+L-i-1, R)
-                i += delta
-                R -= delta
-            elif i-math.ceil(M/2)+1 == L and j>=N//2-L+1: # shift left
-                delta = min(j-N//2+L, R)
-                j -= delta
-                R -= delta
-            elif N//2-j == L and i>=M//2-L+1: # shift up
-                delta = min(i-M//2+L, R)
-                i -= delta
-                R -= delta
-        return (i, j)
+    NUM_LAYERS = min(M, N)//2
 
     matrix = list()
     for i in range(M):
         matrix.append(sys.stdin.readline().split())
 
+    layers = []
+    for i in range(NUM_LAYERS):
+
+        # shave top
+        layer = matrix.pop(0)
+
+        # shave right side
+        layer.extend([row.pop() for row in matrix])
+
+        # shave bottom
+        layer.extend(reversed(matrix.pop()))
+
+        # shave left side
+        layer.extend(reversed([row.pop(0) for row in matrix]))
+
+        # rotate layer
+        r = R % len(layer)
+        layers.append(layer[r:]+layer[:r])
+
+    layers.reverse()
+
+    # build core
+    if M>N:
+        matrix = [layers[0][:2]]
+        remainder = layers[0][2:]
+        while remainder:
+            matrix.append([remainder.pop(), remainder.pop(0)])
+    else:
+        matrix = [layers[0][:len(layers)/2], reversed(layers[0][:len(layers)/2])]
+
+    for layer in layers[1:]:
+
+        m = len(matrix)+2
+        n = len(matrix[0])+2
+
+        # attach top
+        matrix.insert(0, layer[:n])
+
+        # attach right side
+        for i, x in enumerate(layer[n:n+m-2]):
+            matrix[i+1].append(x)
+
+        # attach bottom
+        matrix.append(list(reversed(layer[n+m-2:2*n+m-2])))
+
+        # attach left side
+        for i, x in enumerate(reversed(layer[2*n+m-2:])):
+            matrix[i+1].insert(0, x)
+
     for i in range(M):
         for j in range(N):
-            (old_i, old_j) = shift(i, j, M, N, R)
-            print("{} ".format(matrix[old_i][old_j]), end="")
+            print(matrix[i][j], end=' ')
         print()
 
