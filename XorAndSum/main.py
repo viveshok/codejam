5da0d606644c4fe0db5e9db1c412c17a47b90dec
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2015-07-25 23:27:00 +0800

    progress on xor and sum

--- XorAndSum/main.py
+++ XorAndSum/main.py
@@ -8,13 +8,43 @@ import sys
 #import math
 #import collections
 
+def num_ones(cumulative_binary, left, right):
+    if left < 0:
+        left = 0
+    if right > len(cumulative_binary):
+        right = len(cumulative_binary) - 1
+    if left > right:
+        return 0
+    return cumulative_binary[right] - (cumulative_binary[left-1] if left > 0 else 0)
+
 if __name__ == "__main__":
 
-    a = int(sys.stdin.readline(), 2)
+    N = 314159
+    MOD = 1000000007
 
-    b = int(sys.stdin.readline(), 2)
+    A = sys.stdin.readline()
+    B = sys.stdin.readline()
 
-    answer = ( a ^ ( b * ( 2 ** 314160 - 1 ) ) ) % 1000000007
+    NMax = 1000000
+    A = '0' * (NMax-len(A)) + A
+    B = '0' * (NMax-len(B)) + B
 
-    print(answer)
+    power_mod = [1]
+    for i in range(NMax-1):
+        power_mod.append((2 * power_mod[i]) % MOD)
+
+    count = 0
+    sb = []
+    for i in range(NMax-1, -1, -1):
+        count += 1 if B[i] == '1' else 0
+        sb.append(count)
+
+    ans = 0
+    for i in range(NMax):
+        if A[NMax-1-i] == '1':
+            ans += power_mod[i-1] * (N - num_ones(sb, i-N, i) + 1)
+        else:
+            ans += power_mod[i-1] * num_ones(sb, i-N, i)
+
+    print(ans % MOD)
 
