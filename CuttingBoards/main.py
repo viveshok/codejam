19f117a6f7fe60eace2dd56a1568466eaa8e6f9d
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2015-07-27 00:02:05 +0800

    finally cutting boards without TLE

--- CuttingBoards/main.py
+++ CuttingBoards/main.py
@@ -8,19 +8,29 @@ import sys
 #import math
 #import collections
 
+def cum_sum(xs):
+    sum_ = 0
+    result = list()
+    for x in xs:
+        sum_ += x
+        result.append(sum_)
+    return result
+
 def solve(xs, ys):
+    cum_xs = cum_sum(xs)
+    cum_ys = cum_sum(ys)
     ans = 0
     while xs or ys:
         if not xs:
-            return ans + sum([y for (_i, y) in ys])
+            return ans + cum_ys.pop()
         if not ys:
-            return ans + sum([x for (_i, x) in xs])
-        if xs[-1][1] >= ys[-1][1]:
-            (idx, val) = xs.pop()
-            ans += val + sum([y for (_i, y) in ys])
+            return ans + cum_xs.pop()
+        if xs[-1] >= ys[-1]:
+            cum_xs.pop()
+            ans += xs.pop() + cum_ys[-1]
         else:
-            (idx, val) = ys.pop()
-            ans += val + sum([x for (_i, x) in xs])
+            cum_ys.pop()
+            ans += ys.pop() + cum_xs[-1]
 
 if __name__ == "__main__":
 
@@ -30,9 +40,9 @@ if __name__ == "__main__":
 
         sys.stdin.readline() # skip M & N
 
-        ys = [(i, int(y)) for (i, y) in enumerate(sys.stdin.readline().split())]
-        xs = [(i, int(x)) for (i, x) in enumerate(sys.stdin.readline().split())]
+        ys = sorted([int(y) for y in sys.stdin.readline().split()])
+        xs = sorted([int(x) for x in sys.stdin.readline().split()])
 
-        ans = solve(sorted(xs, key=lambda X: X[1]), sorted(ys, key=lambda Y: Y[1]))
+        ans = solve(xs, ys)
         print(ans % 1000000007)
 
