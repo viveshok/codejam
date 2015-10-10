344d1de48af6e06ac87fdd872025f43ac40a186c
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2015-10-10 17:10:07 +0800

    hacker rank bike racers - almost solved using simplex algo but need to use sequential least squares programming instead because we're minimizing the max, not the sum

--- BikeRacers/main.py
+++ BikeRacers/main.py
@@ -1,36 +1,12 @@
-#!/usr/bin/python3
 
 # how to
 # $ python3 -m doctest -v main.py # to test
 # $ ./main.py < input > output # to run
 
 import sys
-import numpy
+from scipy import optimize
 #import math
 #import collections
-
-# hungarian algorithm for perfect matching of bipartite graph
-
-def lines(matrix, axes):
-    lines_ = list()
-    zeros = matrix == 0
-    while numpy.any(zeros):
-        axis, index, line_ = axes.pop()
-        if axis == 0:
-            zeros[index] = False
-        if axis == 1:
-            zeros[:, index] = False
-        lines_.append((axis, index))
-    return lines_
-
-def all_axes(matrix, N, M):
-    axes = []
-    for i in range(N):
-        axes.append((0, i, matrix[i]))
-    for j in range(M):
-        axes.append((1, j, matrix[:,j]))
-    axes.sort(key=lambda axis: sum([x==0 for x in axis[2]]))
-    return axes
 
 if __name__ == "__main__":
 
@@ -46,24 +22,22 @@ if __name__ == "__main__":
         x, y = [int(X) for X in sys.stdin.readline().split()]
         bikes.append((x, y))
 
-    cost_matrix = numpy.zeros((N, M))
-    for i, biker in enumerate(bikers):
-        for j, bike in enumerate(bikes):
+    c = list()
+    A_ub = list()
+    for i, bike in enumerate(bikes):
+        A_ub.append(i*M*[0]+M*[1]+(M-i-1)*M*[0])
+        for biker in bikers:
             delta_x = biker[0] - bike[0]
             delta_y = biker[1] - bike[1]
-            cost_matrix[i, j] = delta_x * delta_x + delta_y * delta_y
+            c.append(abs(delta_x) + abs(delta_y))
 
-    # step 1
-    aux = cost_matrix - cost_matrix.min(1).reshape(N, 1)
+    b_ub = [1]*M
+    A_eq = [[1]*N*M]
+    b_eq = [K]
+    bounds = [(0, 1) for i in range(M*N)]
 
-    # step 2
-    aux -= aux.min(0)
-
-    # step 3
-    axes = all_axes(aux, N, M)
-    lines_ = lines(aux, axes)
-
-    # step 4
-
-    print(lines_)
+    result = optimize.linprog(c, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq, bounds=bounds)
+    distances = [C*X for C,X in zip(c, result.x)]
+    max_dist = max(distances)
+    print(int(max_dist*max_dist))
 
